e20113f9fc25a24035fcf239160f7b0fccff454c
Vlad-Bakumenko <vlad1star2121@gmail.com>
2026-05-30 19:50:57 +0200

    refactor: extract hardcoded strings and numbers into constants/index.js

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,12 @@
-import {logo} from '../assets';
+import { logo } from "../assets";
+import { GITHUB_URL } from "../constants";
 
 const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
         <nav className='flex justify-between items-center w-full mb-10 pt-3'>
             <img src={logo} alt="sumz_logo" className='w-28 object-contain' />
-            <button className='black_btn' type='button' onClick={() => window.open('https://github.com/Vlad-Bakumenko/ai-summarizer')}>GitHub</button>
+            <button className='black_btn' type='button' onClick={() => window.open(GITHUB_URL)}>GitHub</button>
         </nav>
         <h1 className='head_text'>Summarize Articles with <br className='max-md:hidden' /> <span className='orange_gradient'>OpenAI GPT-4</span></h1>
         <h2 className="desc">Simplify your reading with Summize, an open-source article summarizer that transforms lengthy articles into clear and concise summaries</h2>
